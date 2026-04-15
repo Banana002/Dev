40e27bd98bac97034831737853e72173d0ad35e4
Александр Паутов <sashaplayer4@gmail.com>
2026-04-15 23:15:04 +0300

    Processing exceptions

--- venv/game.py
+++ venv/game.py
@@ -14,13 +14,10 @@ def main():
             column = int(input('Введите номер столбца: '))
             if column < 0 or column >= game.field_size:
                 raise FieldIndexError
-        except FieldIndexError:
-            print(
-                'Значение должно быть неотрицательным и меньше '
-                f'{game.field_size}.'
-            )
-            print('Пожалуйста, введите значения для строки и столбца заново.')
-            continue
+      except ValueError:
+    print('Буквы вводить нельзя. Только числа.')
+    print('Пожалуйста, введите значения для строки и столбца заново.')
+    continue
         else:
             break
 
