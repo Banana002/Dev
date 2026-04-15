4a7abedff540fa737576eda8cb22cd67fea411a4
Александр Паутов <sashaplayer4@gmail.com>
2026-04-15 23:13:22 +0300

    Taming game.py

--- venv/game.py
+++ venv/game.py
@@ -1,20 +1,33 @@
 from gameparts import Board
 from gameparts.exceptions import FieldIndexError
 
+
 def main():
     game = Board()
     game.display()
-    # Пользователь вводит номер строки.
-    row = int(input('Введите номер строки: '))
 
-    column = int(input('Введите номер столбца: '))
-    if column < 0 or column >= game.field_size:
-        raise FieldIndexError
+    while True:
+        try:
+            row = int(input('Введите номер строки: '))
+            if row < 0 or row >= game.field_size:
+                raise FieldIndexError
+            column = int(input('Введите номер столбца: '))
+            if column < 0 or column >= game.field_size:
+                raise FieldIndexError
+        except FieldIndexError:
+            print(
+                'Значение должно быть неотрицательным и меньше '
+                f'{game.field_size}.'
+            )
+            print('Пожалуйста, введите значения для строки и столбца заново.')
+            continue
+        else:
+            break
 
-    column = int(input('Введите номер столбца: '))
     game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
+
 
 if __name__ == '__main__':
     main()
