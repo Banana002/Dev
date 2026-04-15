e3068b54a40af107f9069304e74ceb0f7b0ad162
Александр Паутов <sashaplayer4@gmail.com>
2026-04-15 23:11:16 +0300

    Processing game.py

--- venv/game.py
+++ venv/game.py
@@ -1,8 +1,4 @@
-# game.py
-
 from gameparts import Board
-# Из файла exceptions.py, который лежит в пакете gameparts,
-# импортируется класс FieldIndexError.
 from gameparts.exceptions import FieldIndexError
 
 def main():
@@ -10,12 +6,11 @@ def main():
     game.display()
     # Пользователь вводит номер строки.
     row = int(input('Введите номер строки: '))
-    # Если введённое значение меньше нуля или больше или равно
-    # field_size (это значение равно трём, оно хранится в модуле
-    # parts.py)...
-    if row < 0 or row >= game.field_size:
-        # ...выбросить исключение FieldIndexError.
+
+    column = int(input('Введите номер столбца: '))
+    if column < 0 or column >= game.field_size:
         raise FieldIndexError
+
     column = int(input('Введите номер столбца: '))
     game.make_move(row, column, 'X')
     print('Ход сделан!')
